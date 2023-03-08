6e99b02ce7c47d0435bb6d0d5219d3a1dd5843f1
jyli <lijinyang161@mails.ucas.ac.cn>
2023-03-08 22:12:59 +0800

    connect ui to sqlite db

--- MyTime.py
+++ MyTime.py
@@ -4,6 +4,7 @@ from os import path
 import time
 import json
 import sys
+import sqlite3
 
 
 class Ui_MainWindow(object):
@@ -19,18 +20,31 @@ class Ui_MainWindow(object):
         MainWindow.resize(600, 600)
 
         self.on = False  # 开关标志
-        self.t1 = 0  # 前一次计时时间
+        self.start_time = 0  # 前一次计时时间
 
         localtime = time.localtime(time.time())  # 本地时间
         self.date = '%s-%s-%s' % (localtime.tm_year,
                                   localtime.tm_mon, localtime.tm_mday)  # 转换成日期
 
         # 读取记录
-        self.history = {}
-        self.read_history()
-        if self.date not in self.history.keys():
-            self.history[self.date] = {
-                '总计时': 0, '文献阅读': 0, '技术任务': 0, '日常任务': 0, '服务任务': 0}
+        self.con = sqlite3.connect("data/time_logging.sqlite")
+        self.con.execute(
+            "create table if not exists logging(date, start_time, end_time, duration, class, target, task)")
+
+        cur = self.con.cursor()
+        self.todayLogging = {
+            '总计时': 0, '技术任务': 0, '文献阅读': 0, '日常任务': 0, '服务任务': 0}
+        for row in cur.execute(f"SELECT date, duration, class from logging\
+            where date = '{self.date}'"):
+            self.todayLogging[row[2]] += row[1]
+        self.todayLogging['总计时'] = sum(self.todayLogging.values())
+        target_dict = set()
+        for row in cur.execute(f"SELECT target from logging"):
+            target_dict.add(row[0])
+        task_dict = set()
+        for row in cur.execute(f"SELECT task from logging"):
+            task_dict.add(row[0])
+        cur.close()
 
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
@@ -64,23 +78,15 @@ class Ui_MainWindow(object):
         self.verticalLayout.addWidget(self.classBox)
 
         # https://blog.csdn.net/u012828517/article/details/105158680
-        target_items = [
-            "AAAA",
-            "bbbb"
-        ]
         self.targetBox = QtWidgets.QComboBox()
-        self.targetBox.addItems(target_items)
+        self.targetBox.addItems(list(target_dict))
         # self.targetBox.setCurrentIndex(0)
         self.targetBox.setEditable(True)
         self.targetBox.lineEdit().setPlaceholderText("Target")
         self.targetBox.setFont(font)
         self.verticalLayout.addWidget(self.targetBox)
 
-        wordList = [
-            "taska",
-            "taskb"
-        ]
-        completer = QtWidgets.QCompleter(wordList)
+        completer = QtWidgets.QCompleter(list(task_dict))
         completer.setCaseSensitivity(QtCore.Qt.CaseInsensitive)
         self.taskLine = QtWidgets.QLineEdit()
         self.taskLine.setCompleter(completer)
@@ -109,51 +115,67 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def display(self):
-        totalmin = self.history[self.date]["总计时"]//60
+        totalmin = self.todayLogging["总计时"]//60
         totalhour = totalmin//60
-        workmin = self.history[self.date]['技术任务']//60
+        workmin = self.todayLogging['技术任务']//60
         workhour = workmin//60
-        studymin = self.history[self.date]['文献阅读']//60
+        studymin = self.todayLogging['文献阅读']//60
         studyhour = studymin//60
-        moyumin = self.history[self.date]['日常任务']//60
+        moyumin = self.todayLogging['日常任务']//60
         moyuhour = moyumin//60
-        playmin = self.history[self.date]['服务任务']//60
+        playmin = self.todayLogging['服务任务']//60
         playhour = playmin//60
         self.label_0.setText("今天是%s\n\n技术任务:\t%dh %dmin %ds\n文献阅读:\t%dh %dmin %ds\n日常任务:\t%dh %dmin %ds\n服务任务:\t%dh %dmin %ds\n总计时:\t\t%dh %dmin %ds\n"
                              % (
                                  self.date,
-                                 workhour, workmin % 60, self.history[self.date]['技术任务'] % 60,
-                                 studyhour, studymin % 60, self.history[self.date]['文献阅读'] % 60,
-                                 moyuhour, moyumin % 60, self.history[self.date]['日常任务'] % 60,
-                                 playhour, playmin % 60, self.history[self.date]['服务任务'] % 60,
-                                 totalhour, totalmin % 60, self.history[self.date]["总计时"] % 60,
+                                 workhour, workmin % 60, self.todayLogging['技术任务'] % 60,
+                                 studyhour, studymin % 60, self.todayLogging['文献阅读'] % 60,
+                                 moyuhour, moyumin % 60, self.todayLogging['日常任务'] % 60,
+                                 playhour, playmin % 60, self.todayLogging['服务任务'] % 60,
+                                 totalhour, totalmin % 60, self.todayLogging["总计时"] % 60,
                              ))
 
     def onButtonClick(self):
         if not self.on:
+            # Start the counting
             self.button1.setText('结束')
             self.w.show()
-            self.t1 = time.time()
+            self.start_time = time.time()
             self.label_1.setText('正在计时')
             self.on = True
         else:
+            # Stop the counting and take records
             self.button1.setText('开始')
             self.w.hide()
-            self.history[self.date]["总计时"] += time.time()-self.t1
-            self.history[self.date][self.classBox.currentText()
-                                    ] += time.time()-self.t1
             self.label_1.setText('暂停计时')
-            self.display()
+            self.end_time = time.time()
             self.on = False
-            with open('data/data.json', 'w') as f:
-                json.dump(self.history, f)
 
-    def read_history(self):
-        if path.exists('data/data.json'):
-            with open('data/data.json', 'r') as f:
-                self.history = json.load(f)
+            self.todayLogging["总计时"] += self.end_time-self.start_time
+            self.todayLogging[self.classBox.currentText()
+                              ] += self.end_time-self.start_time
+            self.display()
+
+            cur = self.con.cursor()
+            cur.execute(
+                "insert into logging(date, start_time, end_time, duration, class, target, task) \
+                values (?,?,?,?,?,?,?)",
+                (
+                    self.date,
+                    self.start_time,
+                    self.end_time,
+                    self.end_time-self.start_time,
+                    self.classBox.currentText(),
+                    self.targetBox.currentText(),
+                    self.taskLine.text()))
+            self.con.commit()
+            cur.close()
+            self.con.close()
+            self.con = sqlite3.connect("data/time_logging.sqlite")
 
     def closeEvent(self, event):
+        self.con.close()
+
         for widget in QtWidgets.QApplication.instance().allWidgets():
             if isinstance(widget, QtWidgets.QWidget) and widget != self:
                 widget.close()
